963d965745d77532bc118ab02eabc7d8894b149d
Matt Sherman <matt@jam123.com>
2024-10-09 09:07:21 -0400

    Add links to home page

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,17 @@ export const layout = "layouts/layout.tsx";
 
 export default (data: Lume.Data, helpers: Lume.Helpers) => (
   <>
-    <h1>{data.title}</h1>
     <p>This is the home page for mg5.dev!</p>
+    <ul>
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+      <li>
+        <a href="https://github.com/mattsherman">GitHub</a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/matthewsherman/">LinkedIn</a>
+      </li>
+    </ul>
   </>
 );
