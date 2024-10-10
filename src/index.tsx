b9d4fdbe706e1784f898d4db681221c4db631757
Matt Sherman <matt@jam123.com>
2024-10-10 15:11:57 -0400

    Home page icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-export const title = "Welcome to mg5.dev";
+export const title = "Matt Sherman";
 export const layout = "layouts/layout.tsx";
 
 export default (data: Lume.Data, helpers: Lume.Helpers) => (
   <>
-    <p>This is the home page for mg5.dev!</p>
-    <ul>
-      <li>
+    <ul class="home-page-icons">
+      <li class="home-page-blog-list-item">
         <a href="/blog">Blog</a>
       </li>
-      <li>
+      <li class="home-page-github-list-item">
         <a href="https://github.com/mattsherman">GitHub</a>
       </li>
-      <li>
+      <li class="home-page-linkedin-list-item">
         <a href="https://www.linkedin.com/in/matthewsherman/">LinkedIn</a>
       </li>
     </ul>
