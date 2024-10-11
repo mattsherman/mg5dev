22dacf504ac4190bb62cf78e75cbb7288eb4690b
Matt Sherman <matt@jam123.com>
2024-10-11 07:51:27 -0400

    Change order of link and title in head

--- src/_includes/layouts/post-layout.tsx
+++ src/_includes/layouts/post-layout.tsx
@@ -8,8 +8,8 @@ export default (data: Lume.Data, helpers: Lume.Helpers) => {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <title>{data.title}</title>
         <link rel="stylesheet" href="/styles.css" />
+        <title>{data.title}</title>
       </head>
       <body>
         <nav>
