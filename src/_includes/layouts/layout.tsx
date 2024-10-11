2b5bc10857b7629a2a4ba6a639b14d328931d84c
Matt Sherman <matt@jam123.com>
2024-10-11 07:54:22 -0400

    Add viewport meta

--- src/_includes/layouts/layout.tsx
+++ src/_includes/layouts/layout.tsx
@@ -2,6 +2,7 @@ export default (data: Lume.Data, helpers: Lume.Helpers) => (
   <html lang="en">
     <head>
       <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="/styles.css" />
       <title>{data.title}</title>
     </head>
