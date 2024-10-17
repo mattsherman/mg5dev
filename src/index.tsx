a17d649d12b00640d1506568001115bc26e6b7e8
Matt Sherman <matt@jam123.com>
2024-10-17 09:09:23 -0400

    Hide blog posts link on home page if no posts

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,43 @@
 export const title = "Matt Sherman";
 export const layout = "layouts/layout.tsx";
 
-export default (data: Lume.Data, helpers: Lume.Helpers) => (
-  <>
-    <ul class="home-page-icons">
-      <li>
-        <a href="/blog" class="home-page-blog-link">
-          Blog
-        </a>
-      </li>
-      <li>
-        <a href="/resume" class="home-page-resume-link">
-          Résumé
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/mattsherman" class="home-page-github-link">
-          GitHub
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.linkedin.com/in/matthewsherman/"
-          class="home-page-linkedin-link"
-        >
-          LinkedIn
-        </a>
-      </li>
-    </ul>
-  </>
-);
+export default (data: Lume.Data, helpers: Lume.Helpers) => {
+  const { search } = data;
+
+  const hasPosts = search.pages("type=post").length > 0;
+
+  return (
+    <>
+      <ul class="home-page-icons">
+        {hasPosts && (
+          <li>
+            <a href="/blog" class="home-page-blog-link">
+              Blog
+            </a>
+          </li>
+        )}
+        <li>
+          <a href="/resume" class="home-page-resume-link">
+            Résumé
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/mattsherman"
+            class="home-page-github-link"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/matthewsherman/"
+            class="home-page-linkedin-link"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
