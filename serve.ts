0dbcdf8f5eaf629177be01d53d0c3e144b0177ee
Matt Sherman <matt@jam123.com>
2024-10-17 10:08:37 -0400

    Use expires middleware

--- serve.ts
+++ serve.ts
@@ -1,9 +1,12 @@
 import Server from "lume/core/server.ts";
+import expires from "lume/middlewares/expires.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
+
+server.use(expires());
 
 server.start();
 
