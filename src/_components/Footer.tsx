c29cdbb4cebcbf4f77c246a34a1532c9ae696230
Matt Sherman <matt@jam123.com>
2024-11-21 16:40:16 -0500

    Removed unused params from Footer

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,4 +1,4 @@
-export default (data: Lume.Data, helpers: Lume.Helpers) => (
+export default () => (
   <footer>
     &copy; {new Date().getFullYear()} Matt Sherman.{" "}
     <a href="https://github.com/mattsherman/mg5-dev-web">View source</a>
