4f9c1daac46a253abe7d5d848d12d6a721a9102a
Matt Sherman <matt@jam123.com>
2024-12-05 16:18:16 -0500

    Remove incorrect nested nav

--- src/_includes/layouts/layout.tsx
+++ src/_includes/layouts/layout.tsx
@@ -29,16 +29,14 @@ export default (data: LayoutData, _helpers: Lume.Helpers) => (
     </head>
     <body>
       <nav>
-        <nav>
-          <ol class="header-breadcrumb horizontal-list">
-            <li>
-              <a href="/" class="icon-and-text">
-                <data.comp.icons.HomeIcon />
-                mg5.dev
-              </a>
-            </li>
-          </ol>
-        </nav>
+        <ol class="header-breadcrumb horizontal-list">
+          <li>
+            <a href="/" class="icon-and-text">
+              <data.comp.icons.HomeIcon />
+              mg5.dev
+            </a>
+          </li>
+        </ol>
       </nav>
       <main>
         <header>
