5a593eb863d8313daf11dbf7a8ff950a428ba960
Matt Sherman <matt@jam123.com>
2024-12-07 07:10:40 -0500

    Initial snowfall

--- src/playground/snowfall/embed/script.ts
+++ src/playground/snowfall/embed/script.ts
@@ -1,1 +1,30 @@
-console.log('Hello, world!');
+const body = document.getElementsByTagName('body')[0];
+
+function getRandomInteger(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function createSnowflake(): HTMLElement {
+  const snowflake = document.createElement('div');
+  snowflake.classList.add('snowflake');
+  snowflake.innerHTML = '❄️';
+
+  const left = getRandomInteger(0, 100);
+
+  snowflake.style.left = `${left}%`;
+
+  return snowflake;
+}
+
+function createSnowflakes(num: number): DocumentFragment {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < num; i++) {
+    const snowflake = createSnowflake();
+    fragment.appendChild(snowflake);
+  }
+
+  return fragment;
+}
+
+body.appendChild(createSnowflakes(500));
