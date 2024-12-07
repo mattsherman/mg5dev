ced53266626faf5d756b8ad9463ed3a619716136
Matt Sherman <matt@jam123.com>
2024-12-07 07:19:13 -0500

    Randomize fall duration

--- src/playground/snowfall/embed/script.ts
+++ src/playground/snowfall/embed/script.ts
@@ -4,14 +4,21 @@ function getRandomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function getRandomFloat(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
+
 function createSnowflake(): HTMLElement {
   const snowflake = document.createElement('div');
   snowflake.classList.add('snowflake');
   snowflake.innerHTML = '❄️';
 
   const left = getRandomInteger(0, 100);
+  const fallDuration = getRandomFloat(9, 10);
 
   snowflake.style.left = `${left}%`;
+
+  snowflake.style.setProperty('--fall-duration', `${fallDuration}s`);
 
   return snowflake;
 }
