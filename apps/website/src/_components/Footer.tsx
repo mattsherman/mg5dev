92413ed477e1d69889b80240ce00cd263c6557ca
Matt Sherman <matt@jam123.com>
2024-12-09 13:00:13 -0500

    Change source url in footer

--- apps/website/src/_components/Footer.tsx
+++ apps/website/src/_components/Footer.tsx
@@ -1,6 +1,8 @@
 export default () => (
   <footer>
     &copy; {new Date().getFullYear()} Matt Sherman.{' '}
-    <a href="https://github.com/mattsherman/mg5dev">View source</a>
+    <a href="https://github.com/mattsherman/mg5dev/tree/trunk/apps/website">
+      View source
+    </a>
   </footer>
 );
